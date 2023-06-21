1e1fee0c4d4dde74682197106b6a394954e95adb
Rendy Pratama <rendypratama752@gmail.com>
2023-06-21 22:05:55 +0700

    update procedure pesan

--- procedure.sql
+++ procedure.sql
@@ -19,8 +19,8 @@ end if;
 end <>
 delimiter ;
 
-call validasiLoginViaNoTelp('99999999999','ilham7580');
-call validasiLoginViaNoTelp('08129022310','hhhhhhhhh');
+call validasiLoginViaNoTelp('99999999999','ilham7580'); -- nomor telepon tidak terdaftar
+call validasiLoginViaNoTelp('08129022310','hhhhhhhhh'); -- salah password
 call validasiLoginViaNoTelp('08129022310','ilham7580');
 
 delimiter <>
@@ -44,8 +44,8 @@ end if;
 end <>
 delimiter ;
 
-call validasiLoginViaEmail('hhhhh@gmail.com','ilham7580');
-call validasiLoginViaEmail('ilham@gmail.com','hhhhhhhhh');
+call validasiLoginViaEmail('hhhhh@gmail.com','ilham7580'); -- email belum terdaftar
+call validasiLoginViaEmail('ilham@gmail.com','hhhhhhhhh'); -- password salah
 call validasiLoginViaEmail('ilham@gmail.com','ilham7580');
 
 delimiter <>
@@ -112,71 +112,94 @@ delimiter ;
 call lihatPromoPengguna('budiz');
 call lihatPromoPengguna('lauraz');
 call lihatPromoPengguna('ilhamz');
-call lihatPromoPengguna('santoso');
+call lihatPromoPengguna('santoso'); -- username belum terdaftar
 call lihatPromoPengguna('liliaz');
-
-select*from pengguna;
-select*from produk;
-select*from dompetDigital;
-select*from dompetTerhubung;
-select*from promo;
-select*from promoTerhubung;
-select*from alamat;
-select*from pesanan;
-select*from detailPesanan;
 
 delimiter <>
 create or replace procedure pesan(varUsername varchar(255),varPromo varchar(255),varAlamat varchar(255),
-					   varDompet varchar(255),varTanggal date,listProduk varchar(255),listJumlah varchar(255),varCatatan text)
+					   varMetodePembayaran varchar(255),varTanggal date,listProduk varchar(255),
+                       listJumlah varchar(255),listKurir varchar(255),listPromoPelapak varchar(255),varCatatan text)
 begin
 declare varIdPengguna char(36);
 declare varIdPromo char(36);
 declare varIdAlamat char(36);
-declare varIdDompet char(36);
+declare varIdMetodePembayaran char(36);
 declare varIdProduk char(36);
+declare varIdPelapak char(36);
+declare varIdKurir char(36);
+declare varIdPromoPelapak char(36);
 declare warning varchar(255);
 declare cekKoma int default 1;
 declare i int default 1;
+declare j int default 1;
 declare varProduk varchar(255);
 declare varJumlah varchar(255);
+declare varKurir varchar(255);
+declare varPromoPelapak varchar(255);
 declare cekStok int;
 declare varHarga decimal(10,2);
 declare varMinTransaksi decimal(10,2);
-declare varUUID char(36);
+declare varUUIDPesanan char(36);
+declare varUUIDPelapak char(36);
 declare cekUUID int default 0;
-set varUUID=uuid();
+declare jumlahPromoValid int default 0;
+set varUUIDPesanan=uuid();
 set varIdPengguna=(select id from pengguna where username=varUsername);
-set varIdPromo=(select id from promo join promoTerhubung where promo.id=promoTerhubung.idPromo and idPengguna=varIdPengguna and promo.nama=varPromo);
+set varIdPromo=(select id from promo join promoTerhubung where promo.id=promoTerhubung.idPromo 
+				and idPengguna=varIdPengguna and promo.nama=varPromo);
 set varIdAlamat=(select id from alamat where idPengguna=varIdPengguna and alamat=varAlamat);
-set varIdDompet=(select id from dompetDigital join dompetTerhubung where dompetDigital.id=dompetTerhubung.idDompet and idPengguna=varIdPengguna and dompetDigital.nama=varDompet);
+set varIdMetodePembayaran=(select id from metodePembayaran join metodePembayaranTerhubung 
+						  where metodePembayaran.id=metodePembayaranTerhubung.idMetodePembayaran and idPengguna=varIdPengguna 
+						  and metodePembayaran.nama=varMetodePembayaran);
 if(varIdPengguna is null) then
 	set warning= concat("Username pengguna '",varUsername, "' tidak diketahui");
 	signal sqlstate '44444' set message_text = warning;
 elseif(varIdAlamat is null) then
 	set warning= concat("Username pengguna '",varUsername, "' tidak memiliki alamat '",varAlamat,"'");
 	signal sqlstate '44444' set message_text = warning;
-elseif(varIdDompet is null) then
-	set warning= concat("Username pengguna '",varUsername, "' tidak memiliki dompet '",varDompet,"'");
+elseif(varIdMetodePembayaran is null) then
+	set warning= concat("Username pengguna '",varUsername, "' tidak memiliki metode pembayaran '",varMetodePembayaran,"'");
 	signal sqlstate '44444' set message_text = warning;
 end if;
 create table if not exists Pemberitahuan (pemberitahuan varchar(255));
+create table if not exists PromoPelapakSementara (idPesananPelapak char(36),idPromoPelapak char(36));
 while(cekKoma!=0) do
 -- select cekKoma;
 	set cekKoma=(select locate(',',listProduk,cekKoma+1)); -- cekKoma default 1
     set varProduk=(select substring_index(substring_index(listProduk, ',', i), ',', -1));
     set varIdProduk=(select id from produk where nama=varProduk);
+    set varJumlah=(select substring_index(substring_index(listJumlah, ',', i), ',', -1));
+	set cekStok=(select stok from produk where id=varIdProduk);
 -- select cekKoma, varProduk,varIdProduk,i;
 	if(varIdProduk is not null) then
-        set varJumlah=(select substring_index(substring_index(listJumlah, ',', i), ',', -1));
-		set cekStok=(select stok from produk where id=varIdProduk);
         if(varJumlah<=cekStok) then
-			set cekUUID=1;
-            if((select id from pesanan where id=varUUID) is null) then
-				insert into pesanan(id,idPengguna,idAlamat,idDompet,tanggal,catatan,status) values
-				(varUUID,varIdPengguna,varIdAlamat,varIdDompet,varTanggal,varCatatan,'Sedang diproses');
+			set cekUUID=1; -- memastikan paling tidak ada 1 produk yang valid untuk dibuat pesanan
+            if((select id from pesanan where id=varUUIDPesanan) is null) then -- membatasi pesanan dibuat hanya 1x
+				insert into pesanan(id,idPengguna,idAlamat,idMetodePembayaran,tanggal) values
+				(varUUIDPesanan,varIdPengguna,varIdAlamat,varIdMetodePembayaran,varTanggal);
 			end if;
-			insert into detailPesanan(idPesanan,idProduk,jumlah) values
-			(varUUID,varIdProduk,varJumlah);
+            set varIdPelapak=(select idPelapak from produk where id=varIdProduk);
+            if((select pesananPelapak.id from pesanan join pesananPelapak on pesanan.id=pesananPelapak.idPesanan 
+            where pesanan.id=varUUIDPesanan and pesananPelapak.idPelapak=varIdPelapak) is null) then -- membatasi sebuah pesanan tidak memiliki pelapak ganda
+				set varUUIDPelapak=uuid();
+				set varKurir=(select substring_index(substring_index(listKurir, ',', j), ',', -1));
+                set varIdKurir=(select id from kurir where nama=varKurir);
+                if(varIdKurir is not null) then
+					insert into pesananPelapak(id,idPesanan,idPelapak,idKurir,status,catatan) 
+					values (varUUIDPelapak,varUUIDPesanan,varIdPelapak,varIdKurir,'Sedang diproses',varCatatan);
+                else
+					insert into Pemberitahuan values (concat('Kurir = ', varKurir,' tidak diketahui, set default ke kurir BukaExpress REG'));
+					insert into pesananPelapak(id,idPesanan,idPelapak,idKurir,status,catatan) 
+					values (varUUIDPelapak,varUUIDPesanan,varIdPelapak,(select id from kurir where nama='BukaExpress REG'),'Sedang diproses',varCatatan);
+                end if;
+                set varPromoPelapak=(select substring_index(substring_index(listPromoPelapak, ',', j), ',', -1));
+                set varIdPromoPelapak=(select id from promoPelapak where idPelapak=varIdPelapak and nama=varPromoPelapak);
+                if(varIdPromoPelapak is not null) then
+					insert into PromoPelapakSementara values (varUUIDPelapak,varIDPromoPelapak);
+				end if;
+                set j=j+1;
+            end if;
+			insert into detailPesanan(idPesananPelapak,idProduk,jumlah) values (varUUIDPelapak,varIdProduk,varJumlah);
 			update produk set stok=stok-varJumlah,terjual=terjual+varJumlah where id=varIdProduk;
 		else
 			insert into Pemberitahuan values (concat('Stok ',varProduk,' tidak mencukupi'));
@@ -185,15 +208,28 @@ while(cekKoma!=0) do
 		insert into Pemberitahuan values (concat(varProduk,' tidak diketahui'));
 	end if;
     set i=i+1;
-    select i;
 end while;
-set varHarga=(select produk.harga*detailPesanan.jumlah from pesanan left join detailPesanan on pesanan.id=detailPesanan.idPesanan
-join produk on detailPesanan.idProduk=produk.id where pesanan.id=(select id from pesanan order by id desc limit 1) group by pesanan.id);
+while(jumlahPromoValid<(select count(*) from PromoPelapakSementara)) do
+	set varUUIDPelapak=(select idPesananPelapak from PromoPelapakSementara limit jumlahPromoValid,1);
+	set varHarga=(select sum(produk.harga*detailPesanan.jumlah) from pesananPelapak left join detailPesanan 
+				on pesananPelapak.id=detailPesanan.idPesananPelapak join produk on detailPesanan.idProduk=produk.id 
+                where pesananPelapak.id=varUUIDPelapak);
+	set varIdPromoPelapak=(select idPromoPelapak from PromoPelapakSementara limit jumlahPromoValid,1);
+    select*from promopelapaksementara;
+    select varUUIDPelapak,varIdPromoPelapak;
+	set varMinTransaksi=(select minTransaksi from promoPelapak where id=varIdPromoPelapak);
+	if(varHarga>=varMinTransaksi) then
+		update pesananPelapak set idPromoPelapak=varIdPromoPelapak where id=varUUIDPelapak;
+	else
+		insert into Pemberitahuan values (concat('Total harga pesanan pada salah satu pelapak = ', varHarga, '> kriteria min transaksi ', varPromoPelapak, '= ',varMinTransaksi));
+	end if;
+set jumlahPromoValid=jumlahPromoValid+1;
+end while;
 set varMinTransaksi=(select minTransaksi from promo where id=varIdPromo);
 if(cekUUID=1) then -- jika terdapat pesanan barang yang valid
 	if(varIdPromo is not null) then
 		if(varHarga>=varMinTransaksi) then
-			update pesanan set idPromo=varIdPromo where id=(select id from pesanan order by id desc limit 1);
+			update pesanan set idPromo=varIdPromo where id=varUUIDPesanan;
 			delete from promoTerhubung where idPengguna=varIdPengguna and idPromo=varIdPromo;
 		else
 			insert into Pemberitahuan values (concat('Total harga pesanan = ', varHarga, '> kriteria min transaksi ', varPromo, '= ',varMinTransaksi));
@@ -202,17 +238,36 @@ if(cekUUID=1) then -- jika terdapat pesanan barang yang valid
 end if;
 select*from Pemberitahuan;
 drop table if exists Pemberitahuan;
+drop table if exists PromoPelapakSementara;
 end <>
 delimiter ;
-select*from promo;
+SELECT*FROM metodepembayaranterhubung;
+select*from pelapak;
+select*from promopelapak;
+select*from produk;
+select*from pesanan;
+select*from pesananPelapak;
+select*from detailpesanan;
+select*from kurir;
 
-call pesan('hafiz','','Jl. Siput No. 123','OVO','2023-06-07','Headphone Sony',1,'');
-call pesan('ilhamz','','Jl. Siput No. 123','OVO','2023-06-07','Headphone Sony',1,'');
-call pesan('ilhamz','','Jl. Rambutan No. 123','GoPay','2023-06-07','Headphone Sony',1,'');
-call pesan('ilhamz','','Jl. Rambutan No. 123','OVO','2023-06-07','Headphone Asus',1,'');
-call pesan('ilhamz','','Jl. Rambutan No. 123','OVO','2023-06-07','Headphone Sony',1,'Cepat kirim');
-call pesan('ilhamz','Promo Lebaran','Jl. Rambutan No. 123','OVO','2023-06-07','Headphone Sony',2,'Cepat kirim');
-call pesan('ilhamz','Promo Lebaran','Jl. Rambutan No. 123','OVO','2023-06-07','Kemeja Denim',3,'');
-call pesan('budiz','Promo Lebaranz','Jl. Mangga No. 456','BCA','2023-06-08','Laptop Asus ZenForce',1,'');
-call pesan('budiz','Promo Lebaran','Jl. Mangga No. 456','GoPay','2023-06-08','Sepatu Nike Running,Kemeja Denim','1, 2','');
-call pesan('budiz','Promo Lebaran','Jl. Mangga No. 456','GoPay','2023-06-09','Headphone Sony,Kemeja Denim,Laptop Asus ZenForce,Carolina Herera,Sepatu Nike Running,Baju Renang','2,4,1,3,30,2','');
+select sum(produk.harga*detailPesanan.jumlah) from pesananPelapak left join pesanan on pesanan.id=pesananPelapak.idPesanan 
+                join detailPesanan on pesananPelapak.id=detailPesanan.idPesananPelapak join produk on detailPesanan.idProduk=produk.id 
+                where pesanan.id='631c2e6f-1034-11ee-b1a1-6802ce07b577' and pesananPelapak.idPelapak='5fe3f7bd-0f81-11ee-813d-4ceab0b3711e';
+select sum(produk.harga*detailPesanan.jumlah) from pesananPelapak left join pesanan on pesanan.id=pesananPelapak.idPesanan 
+                join detailPesanan on pesananPelapak.id=detailPesanan.idPesananPelapak join produk on detailPesanan.idProduk=produk.id 
+                where pesananPelapak.id='fefbea1f-1037-11ee-b1a1-6802ce07b577';
+
+select*from lihatSemuaPesanan;
+call pesan('hafiz','','Jl. Siput No. 123','OVO','2023-06-07','Headphone Sony',1,'SiCepat HALU','Promo',''); -- username tidak diketahui
+call pesan('ilhamz','','Jl. Siput No. 123','OVO','2023-06-07','Headphone Sony',1,'SiCepat REG','Promo',''); -- alamat tidak deketahui pada pengguna terkait
+call pesan('ilhamz','','Jl. Rambutan No. 123','GoPay','2023-06-07','Headphone Sony',1,'BukaExpress REG','Promo',''); -- metode pembayaran tidak diketahui pada pengguna terkait
+call pesan('ilhamz','','Jl. Rambutan No. 123','OVO','2023-06-07','Headphone Asus',1,'J&t REG','Promo Lebaran',''); -- produk tidak diketahui
+call pesan('ilhamz','','Jl. Rambutan No. 123','OVO','2023-06-07','Headphone Sony',1,'J&t REG','Promo Lebaran','Cepat kirim');
+call pesan('budiz','Promo lebaran','Jl. Mangga No. 456','Gopay','2023-06-07','Headphone Sony',1,'J&t REG','Potongan harga','Cepat kirim'); -- 
+call pesan('ilhamz','Promo Lebaran','Jl. Rambutan No. 123','OVO','2023-06-07','Headphone Sony',2,'J&t','Potongan harga','Cepat kirim');
+call pesan('ilhamz','Promo Lebaran','Jl. Rambutan No. 123','OVO','2023-06-07','Kemeja Denim',3,'SiCepat REG','Potongan harga','');
+call pesan('budiz','Promo Lebaranz','Jl. Mangga No. 456','BCA','2023-06-08','Laptop Asus ZenForce',1,'J&t','Potongan harga','');
+call pesan('budiz','Promo Lebaran','Jl. Mangga No. 456','GoPay','2023-06-08','Sepatu Nike Running,Kemeja Denim','1, 2','SiCepat HALU,J&T REG','Potongan harga,Potongan harga','');
+call pesan('budiz','Promo Lebaran','Jl. Mangga No. 456','GoPay','2023-06-09',
+'Headphone Sony,Kemeja Denim,Laptop Asus ZenForce,Carolina Herera,Sepatu Nike Running,Baju Renang',
+'2,4,1,3,30,2','SiCepat HALU,J&T REG,BukaExpress REG,J&T REG','Potongan harga,Potongan harga,Promo Lebaran,Potongan harga','');
