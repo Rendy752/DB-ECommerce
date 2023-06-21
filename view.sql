b9db253fb27fc9e2bc2d5c9c1ef3f72fe3945320
Rendy Pratama <rendypratama752@gmail.com>
2023-06-22 00:08:44 +0700

    update view

--- view.sql
+++ view.sql
@@ -8,20 +8,25 @@ from produk join pelapak join produkFavorit where produk.idPelapak = pelapak.id 
 group by produk.nama order by count(idProduk) desc limit 3;
 select*from top3ProdukFavorit;
 
-create or replace view lihatSemuaPesanan as select pesanan.id as `ID`,pengguna.namaLengkap as `Nama Pengguna`,
-sum(detailPesanan.jumlah*produk.harga) as `Total Harga Awal`,coalesce(concat(promo.diskon,'%'),'0%') as `Diskon`,
-round(substring_index(coalesce(sum((detailPesanan.jumlah*produk.harga))*(promo.diskon/100),0),'.',2),2) as `Potongan`,
-round(coalesce(sum((detailPesanan.jumlah*produk.harga)-((detailPesanan.jumlah*produk.harga)*(promo.diskon/100))),
-sum(detailPesanan.jumlah*produk.harga)),2) as `Total Harga Akhir`, pesanan.tanggal as `Tanggal`
-from pesanan left join detailPesanan on detailPesanan.idPesanan=pesanan.id join produk on detailPesanan.idProduk=produk.id
-join pengguna on pesanan.idPengguna=pengguna.id left join promo on pesanan.idPromo=promo.id
+create or replace view lihatSemuaPesanan as select pesanan.id as `No. Pesanan`,pesanan.tanggal as `Tanggal Pemesanan`,
+date(tanggal+8) as `Estimasi Sampai`,alamat.namaPenerima as `Nama Penerima`,
+concat(alamat,', ',kecamatan,', ',kota,'. ',provinsi,', ',kodePos,'.') as `Alamat`,alamat.noTelp as `No. Telp`,
+metodePembayaran.nama as `Metode Pembayaran`,sum(detailPesanan.jumlah*produk.harga) as `Total Harga Barang`,
+concat('Rp',round(substring_index(coalesce(sum((detailPesanan.jumlah*produk.harga))*(promo.diskon/100),0),'.',2),2)) as `Potongan Extra`,
+sum(kurir.ongkir) as `Ongkos Kirim`,concat('Rp',round(coalesce(sum((detailPesanan.jumlah*produk.harga)-((detailPesanan.jumlah*produk.harga)*
+(promo.diskon/100))+kurir.ongkir),sum(detailPesanan.jumlah*produk.harga)+sum(kurir.ongkir)),2)) as `Jumlah Bayar`, 
+pesanan.tanggal as `Tanggal` from pesanan left join pesananPelapak on pesananPelapak.idPesanan=pesanan.id join detailPesanan 
+on detailPesanan.idPesananPelapak=pesananPelapak.id join produk on detailPesanan.idProduk=produk.id join pengguna 
+on pesanan.idPengguna=pengguna.id left join promo on pesanan.idPromo=promo.id join kurir on kurir.id=pesananPelapak.idKurir
+join alamat on pesanan.idAlamat=alamat.id join metodePembayaran on metodePembayaran.id=pesanan.idMetodePembayaran 
 group by pesanan.id order by pesanan.tanggal asc;
 select*from lihatSemuaPesanan;
 
 create or replace view lihatSemuaPesananProduk as select produk.nama as `Nama Produk`,alamat.namaPenerima as `Nama Penerima`,
 detailPesanan.jumlah as `Jumlah`, detailPesanan.jumlah*produk.Harga as `Total`,pesanan.tanggal as `Tanggal`
-from detailPesanan left join pesanan on detailPesanan.idPesanan=pesanan.id join produk on detailPesanan.idProduk=produk.id
-join pengguna on pesanan.idPengguna=pengguna.id join alamat on pesanan.idAlamat=alamat.id order by pesanan.tanggal asc;
+from detailPesanan left join pesananPelapak on detailPesanan.idPesananPelapak=pesananPelapak.id join pesanan 
+on pesananPelapak.idPesanan=pesanan.id join produk on detailPesanan.idProduk=produk.id join pengguna 
+on pesanan.idPengguna=pengguna.id join alamat on pesanan.idAlamat=alamat.id order by pesanan.tanggal asc;
 select*from lihatSemuaPesananProduk;
 
 create or replace view top3ProdukTerlaris as select produk.nama as `Nama Produk`,pelapak.nama as `Nama Pelapak`,
